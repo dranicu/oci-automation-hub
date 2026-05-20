9124e4c65f5cbfe1dd894a127305aa2ff80f2cd6
kumvijak <vijay.bd.kumar@oracle.com>
2026-05-20 20:09:30 +0530

    file header corrected for mcp and ui-server app files.
    file header corrected for mcp and ui-server app files.

--- genai-cloudops-agent-sample/instance-images/ui-server/app.py
+++ genai-cloudops-agent-sample/instance-images/ui-server/app.py
@@ -1,6 +1,7 @@
-#!/usr/bin/env python3
 # Copyright (c) 2024, 2026, Oracle and/or its affiliates. All rights reserved.
 # The Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl/
+#!/usr/bin/env python3
+
 from __future__ import annotations
 
 import contextlib
