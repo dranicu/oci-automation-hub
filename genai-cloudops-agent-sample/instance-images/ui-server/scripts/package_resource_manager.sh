2903630a6cf76c489da9501ee0b15a1236f6e170
kumvijak <vijay.bd.kumar@oracle.com>
2026-06-01 17:59:16 +0530

    fixed headers for html, py and sh files.
    fixed headers for html, py and sh files.

--- genai-cloudops-agent-sample/instance-images/ui-server/scripts/package_resource_manager.sh
+++ genai-cloudops-agent-sample/instance-images/ui-server/scripts/package_resource_manager.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env sh
+#!/bin/bash
+
 # Copyright (c) 2024, 2026, Oracle and/or its affiliates. All rights reserved.
 # The Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl/
+
 set -eu
 
 ROOT_DIR="$(CDPATH= cd -- "$(dirname -- "$0")/.." && pwd)"
