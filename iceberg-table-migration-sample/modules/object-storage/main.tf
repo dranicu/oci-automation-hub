fa2f1822b0b9dc11c4ab56944512d530503242ec
Adina Ion-Nicolescu <adina.nicolescu@oracle.com>
2026-06-23 17:17:49 +0300

    Add files via upload

--- iceberg-table-migration-sample/modules/object-storage/main.tf
+++ iceberg-table-migration-sample/modules/object-storage/main.tf
@@ -1,5 +1,5 @@
-// Copyright (c) 2021, Oracle and/or its affiliates. All rights reserved.
-// Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+# Copyright (c) 2024, 2026, Oracle and/or its affiliates. All rights reserved.
+# The Universal Permissive License (UPL), Version 1.0 as shown at https://oss.oracle.com/licenses/upl/
 data "oci_objectstorage_namespace" "this" {
   compartment_id = var.oci_provider["tenancy_ocid"]
 }
